74c0e1cc3fcb03670ed70d35833487d07229d1f6
Isac-SS <isacsants679@gmail.com>
2022-12-13 15:07:13 -0300

    folder fix commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #cdcdcd;
+    background-color: #00AAF0;
+    color: #FFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.4;
+    }
+`
+
+
+
+
